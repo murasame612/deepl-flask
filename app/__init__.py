fe7aad13946deb1d9714a2c63ac1a71afd5f17e0
HongWen <1084199458@qq.com>
2024-12-19 13:53:21 +0800

    基本功能建立

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+import flask
+from .route import apr
+
+def create_app():
+    app = flask.Flask(__name__)
+    app.secret_key='avsdfasf-absdfasf-1234'
+    app.config['UPLOAD_FOLDER'] = 'uploads/'
+    app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+    app.register_blueprint(apr)
+    return app
---
+++ app/route.py
@@ -0,0 +1,116 @@
+from flask import Blueprint, flash, render_template, request, jsonify, redirect, url_for, session,current_app
+from werkzeug.utils import secure_filename
+import os
+import numpy as np
+import cv2
+import base64
+
+apr = Blueprint('apr', __name__)
+users_db={
+    "admin": {"password": "admin"},
+}
+
+@apr.before_request
+def check_login():
+    # 如果用户未登录并且请求的不是登录或注册路由，则重定向到登录页面
+    if 'username' not in session and request.endpoint not in ['apr.login', 'apr.logout','apr.register']:
+        return redirect(url_for('apr.login'))
+    
+@apr.route('/')
+def index():
+    return redirect(url_for('apr.main'))
+
+@apr.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = users_db.get(username)
+        if user and user['password'] == password:
+            session['username'] = username
+            return jsonify({'success': True})
+        else:
+            return jsonify({'success': False}), 401
+    return render_template('login.html')
+
+@apr.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        if username in users_db:
+            return jsonify({'success': False, 'message': '用户名已存在'}), 400
+
+        # 保存用户数据
+        users_db[username] = {'password': password}
+        return jsonify({'success': True})
+
+    return render_template('register.html')
+
+@apr.route('/main')
+def main():
+    user = session.get('username')
+    return render_template('main.html', user=user)
+
+
+@apr.route('/logout', methods=['POST'])
+def logout():
+    # 清除 session 中的用户名
+    session.pop('username', None)
+    print("已注销")
+    return redirect(url_for('apr.login'))
+
+# 检查文件扩展名是否被允许
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
+
+@apr.route('/upload_image', methods=['POST'])
+def upload_image():
+    """
+    处理上传的图像文件，并返回处理后的图像和相关信息。
+
+    该函数执行以下步骤：
+    1. 检查请求中是否包含图像文件。
+    2. 验证文件名是否为空。
+    3. 使用 OpenCV 读取图像文件并进行处理。
+    4. 将处理后的图像转换为 base64 编码。
+    5. 返回处理后的图像和相关文本信息。
+    """
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'}), 400
+    
+    file = request.files['image']
+    selection = session.get('selection')
+    print(selection)
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    file_bytes = np.frombuffer(file.read(), np.uint8)
+
+    # 使用 OpenCV 解码为图像
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    #-------------------------------------#
+    #image是一个cv2图片对象                  
+    #模型处理返回图片
+    # 将返回的图片在下一行代码输入            
+    #-------------------------------------#
+    
+    convert_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)#转换颜色通道
+    convert_image = cv2.threshold(convert_image, 127, 255, cv2.THRESH_BINARY)[1]
+    
+    # 保存图片为jpeg
+    _, buffer = cv2.imencode('.jpg', convert_image)#image更换为你输入的cv2图片对象
+
+    # 将图片编码为 base64 字符串
+    encoded_string = base64.b64encode(buffer).decode('utf-8')
+
+    # 假设返回的一张图片和两个字符串
+    #返回base64编码后的图片和文本
+    return jsonify({
+        'imageUrl': encoded_string
+    })
